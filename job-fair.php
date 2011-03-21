387e61bb1af65d71134bc5849c99902e47445f33
Robert Burke <burke5@yt.acm.uiuc.edu>
2011-03-21 01:07:28 -0500

    added job-fair.php

--- job-fair.php
+++ job-fair.php
@@ -1,3 +1,10 @@
+<?php
+
+if(!isset($_GET['code'])) {
+  header('Location: ./');
+}
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -10,6 +17,7 @@
 </head>
 
 <body>
+
 	<div id="wrapper">
     	<div id="header">
         	<div id="logo">
