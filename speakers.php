4e5ae32dc9dd7410ce472ed9f863501ab43e4e65
Hristo Oskov <HristoOskov@mobile-103-104.near.uiuc.edu>
2011-08-25 14:37:07 -0500

    speakers page images

--- speakers.php
+++ speakers.php
@@ -21,7 +21,7 @@
 							<li class="speaker">
 								<div class="speaker-pic">
 									<!-- <img src="images/speakers/CliffClick.jpg" alt="Cliff Click" width="150" height="150" /> -->
-									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" />
+									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" style="margin-top: 28px" />
 								</div>
 								<p class="speaker-name">Cliff Click</p>
 								<p class="speaker-affiliation">Distinguished Engineer, Azul Systems</p>
@@ -29,7 +29,7 @@
 							<li class="speaker">
 								<div class="speaker-pic">
 									<!-- <img src="images/speakers/JoshBloch.jpg" alt="Josh Bloch" width="150" height="150" /> -->
-									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" />
+									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" style="margin-top: 28px" />
 								</div>
 								<p class="speaker-name">Josh Bloch</p>
 								<p class="speaker-affiliation">Chief Java Architect, Google</p>
@@ -37,7 +37,7 @@
 							<li class="speaker">
 								<div class="speaker-pic">
 									<!-- <img src="images/speakers/JonHall.jpg" alt="Jon 'maddog' Hall" width="150" height="150" /> -->
-									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" />
+									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" style="margin-top: 28px" />
 								</div>
 								<p class="speaker-name">Jon 'maddog' Hall</p>
 								<p class="speaker-affiliation">Executive Director, Linux International</p>
@@ -45,7 +45,7 @@
 							<li class="speaker">
 								<div class="speaker-pic">
 									<!-- <img src="images/speakers/MarkRussinovich.jpg" alt="Mark Russinovich" width="150" height="150" /> -->
-									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" />
+									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" style="margin-top: 28px" />
 								</div>
 								<p class="speaker-name">Mark Russinovich</p>
 								<p class="speaker-affiliation">Technical Fellow, Microsoft</p>
@@ -53,7 +53,7 @@
 							<li class="speaker clear">
 								<div class="speaker-pic">
 									<!-- <img src="images/speakers/RachaelBrady.jpg" alt="Rachael Brady" width="150" height="150" /> -->
-									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" />
+									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" style="margin-top: 28px" />
 								</div>
 								<p class="speaker-name">Rachael Brady</p>
 								<p class="speaker-affiliation">HCI/Visualizations Researcher, Duke University</p>
@@ -61,7 +61,7 @@
 							<li class="speaker">
 								<div class="speaker-pic">
 									<!-- <img src="images/speakers/ScottKlemmer.jpg" alt="Scott Klemmer" width="150" height="150" /> -->
-									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" />
+									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" style="margin-top: 28px" />
 								</div>
 								<p class="speaker-name">Scott Klemmer</p>
 								<p class="speaker-affiliation">HCI Researcher, Stanford University</p>
@@ -69,7 +69,7 @@
 							<li class="speaker">
 								<div class="speaker-pic">
 									<!-- <img src="images/speakers/MarkMakdad.jpg" alt="Mark Makdad" width="150" height="150" /> -->
-									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" />
+									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" style="margin-top: 28px" />
 								</div>
 								<p class="speaker-name">Mark Makdad</p>
 								<p class="speaker-affiliation">Director/Founder, Long Weekend LLC</p>
@@ -77,7 +77,7 @@
 							<li class="speaker">
 								<div class="speaker-pic">
 									<!-- <img src="images/speakers/BenKamens.jpg" alt="Ben Kamens" width="150" height="150" /> -->
-									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" />
+									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" style="margin-top: 28px" />
 								</div>
 								<p class="speaker-name">Ben Kamens</p>
 								<p class="speaker-affiliation">Lead Developer, Khan Academy</p>
@@ -85,7 +85,7 @@
 							<li class="speaker">
 								<div class="speaker-pic">
 									<!-- <img src="images/speakers/DouglasHofstadter.jpg" alt="Douglas Hofstadter" width="150" height="150" /> -->
-									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" />
+									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" style="margin-top: 28px" />
 								</div>
 								<p class="speaker-name">Douglas Hofstadter</p>
 								<p class="speaker-affiliation">Professor of Cognitive Science, Indiana University</p>
@@ -93,7 +93,7 @@
 							<li class="speaker">
 								<div class="speaker-pic">
 									<!-- <img src="images/speakers/BrianMurphy.jpg" alt="Brian Murphy" width="150" height="150" /> -->
-									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" />
+									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" style="margin-top: 28px" />
 								</div>
 								<p class="speaker-name">Brian Murphy</p>
 								<p class="speaker-affiliation">Editor, Dorkly.com</p>
@@ -101,7 +101,7 @@
 							<li class="speaker">
 								<div class="speaker-pic">
 									<!-- <img src="images/speakers/FredGallagher.jpg" alt="Fred Gallagher" width="150" height="150" /> -->
-									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" />
+									<img src="images/speakers/placeholder.png" alt="placeholder" width="150" style="margin-top: 28px" />
 								</div>
 								<p class="speaker-name">Fred Gallagher</p>
 								<p class="speaker-affiliation">Co-Creator/Cartoonist, Megatokyo</p>
