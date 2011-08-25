0bac22dcfc33c7a73d2aabf512bbd1ab01c8005a
Bhargav Nookala <nooknb@gmail.com>
2011-08-25 14:33:42 -0500

    placeholder image

--- speakers.php
+++ speakers.php
@@ -20,76 +20,77 @@ include('header.php');
 						<ul>
 							<li class="speaker">
 								<div class="speaker-pic">
-									<!-- <img src="images/speakers/CliffClick.jpg" alt="Cliff Click" width="150" height="150" /> -->
+									<img src="images/placeholder.png" alt="Cliff Click" width="150" height="150" />
 								</div>
 								<p class="speaker-name">Cliff Click</p>
 								<p class="speaker-affiliation">Distinguished Engineer, Azul Systems</p>
 							</li>
 							<li class="speaker">
 								<div class="speaker-pic">
-									<!-- <img src="images/speakers/JoshBloch.jpg" alt="Josh Bloch" width="150" height="150" /> -->
+									<img src="images/placeholder.png" alt="Josh Bloch" width="150" height="150" />
 								</div>
 								<p class="speaker-name">Josh Bloch</p>
 								<p class="speaker-affiliation">Chief Java Architect, Google</p>
 							</li>
 							<li class="speaker">
 								<div class="speaker-pic">
-									<!-- <img src="images/speakers/JonHall.jpg" alt="Jon 'maddog' Hall" width="150" height="150" /> -->
+									<img src="images/placeholder.png" alt="John 'maddog' Hall" width="150" height="150" />
 								</div>
 								<p class="speaker-name">Jon 'maddog' Hall</p>
 								<p class="speaker-affiliation">Executive Director, Linux International</p>
 							</li>
 							<li class="speaker">
 								<div class="speaker-pic">
-									<!-- <img src="images/speakers/MarkRussinovich.jpg" alt="Mark Russinovich" width="150" height="150" /> -->
+									<img src="images/placeholder.png" alt="Mark Russinovich" width="150" height="150" />
 								</div>
 								<p class="speaker-name">Mark Russinovich</p>
 								<p class="speaker-affiliation">Technical Fellow, Microsoft</p>
 							</li>
 							<li class="speaker clear">
 								<div class="speaker-pic">
-									<!-- <img src="images/speakers/RachaelBrady.jpg" alt="Rachael Brady" width="150" height="150" /> -->
+									<img src="images/placeholder.png" alt="Rachel Brady" width="150" height="150" />
 								</div>
 								<p class="speaker-name">Rachael Brady</p>
 								<p class="speaker-affiliation">HCI/Visualizations Researcher, Duke University</p>
 							</li>
 							<li class="speaker">
 								<div class="speaker-pic">
-									<!-- <img src="images/speakers/ScottKlemmer.jpg" alt="Scott Klemmer" width="150" height="150" /> -->
+									<img src="images/placeholder.png" alt="Scott Klemmer" width="150" height="150" />
 								</div>
 								<p class="speaker-name">Scott Klemmer</p>
 								<p class="speaker-affiliation">HCI Researcher, Stanford University</p>
 							</li>
 							<li class="speaker">
 								<div class="speaker-pic">
-									<!-- <img src="images/speakers/MarkMakdad.jpg" alt="Mark Makdad" width="150" height="150" /> -->
+									<img src="images/placeholder.png" alt="Mark Makdad" width="150" height="150" />
 								</div>
 								<p class="speaker-name">Mark Makdad</p>
 								<p class="speaker-affiliation">Director/Founder, Long Weekend LLC</p>
 							</li>
 							<li class="speaker">
 								<div class="speaker-pic">
-									<!-- <img src="images/speakers/BenKamens.jpg" alt="Ben Kamens" width="150" height="150" /> -->
+									<img src="images/placeholder.png" alt="Ben Kamens" width="150" height="150" />
 								</div>
 								<p class="speaker-name">Ben Kamens</p>
 								<p class="speaker-affiliation">Lead Developer, Khan Academy</p>
 							</li>
 							<li class="speaker">
 								<div class="speaker-pic">
-									<!-- <img src="images/speakers/DouglasHofstadter.jpg" alt="Douglas Hofstadter" width="150" height="150" /> -->
+									<img src="images/placeholder.png" alt="Douglas Hofstadter" width="150" height="150" />
 								</div>
 								<p class="speaker-name">Douglas Hofstadter</p>
 								<p class="speaker-affiliation">Professor of Cognitive Science, Indiana University</p>
 							</li>
 							<li class="speaker">
 								<div class="speaker-pic">
-									<!-- <img src="images/speakers/BrianMurphy.jpg" alt="Brian Murphy" width="150" height="150" /> -->
+									<img src="images/placeholder.png" alt="Brian Murphy" width="150" height="150" />
 								</div>
 								<p class="speaker-name">Brian Murphy</p>
 								<p class="speaker-affiliation">Editor, Dorkly.com</p>
 							</li>
 							<li class="speaker">
 								<div class="speaker-pic">
+									<img src="images/placeholder.png" alt="Fred Gallagher" width="150" height="150" />
 									<!-- <img src="images/speakers/FredGallagher.jpg" alt="Fred Gallagher" width="150" height="150" /> -->
 								</div>
 								<p class="speaker-name">Fred Gallagher</p>
