011143569d61829b57323e88892d3c4b4091e9bd
Bhargav Nookala <nooknb@gmail.com>
2011-08-31 19:57:07 -0500

    styled registration page

--- register.php
+++ register.php
@@ -7,19 +7,20 @@ include('header.php');
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>About</title>
     
-	<link href="css/styles.css" rel="stylesheet" type="text/css" />
+    <link href="css/styles.css" rel="stylesheet" type="text/css" />
     <link href="css/menu.css" rel="stylesheet" type="text/css" />
-	<link href="css/footer.css" rel="stylesheet" type="text/css" />
+    <link href="css/footer.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 	<div id="wrapper">
-		<?php getHeader(); ?>
-        <div id="content">
-        	<h1>Register for Reflections | Projections</h1>
-		<div style="width:100%; text-align:left;" ><iframe  src="http://www.eventbrite.com/tickets-external?eid=1147561387&ref=etckt" frameborder="0" height="224" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe><div style="font-family:Helvetica, Arial; font-size:10px; padding:5px 0 5px; margin:2px; width:100%; text-align:left;" ><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.com/features?ref=etckt" >Online event registration</a><span style="color:#ddd;" > for </span><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://rp.eventbrite.com?ref=etckt" >Reflections | Projections 2011</a><span style="color:#ddd;" > powered by </span><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.com?ref=etckt" >Eventbrite</a></div></div>
+	    <?php getHeader(); ?>
+	    <div id="content">
+		<div style="width:100%; text-align:left;" >
+		    <iframe src="http://www.eventbrite.com/tickets-external?eid=1147561387&ref=etckt" scrolling="no" frameborder="0" height="224" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+		</div>
         </div>
     </div>
-	<?php include('footer.php') ?>
+    <?php include('footer.php') ?>
 </body>
 </html>
