9c65c0d400277044bc6c1ae3cabf3ffcb1e26a2f
Bhargav Nookala <nooknb@gmail.com>
2011-08-31 22:57:41 -0500

    fixed registration css again

--- register.php
+++ register.php
@@ -15,10 +15,15 @@ include('header.php');
 <body>
 	<div id="wrapper">
 	    <?php getHeader(); ?>
-	    <div id="content">
-		<div style="width:100%; text-align:left;" >
-		    <iframe src="http://www.eventbrite.com/tickets-external?eid=1147561387&ref=etckt" scrolling="no" frameborder="0" height="224" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+	    <div id="content-wrapper">
+		<h1 class="registration-heading vertical-text">registration</h1>
+		<div id="content">
+		    <p>Walk-up registration is available, but does not guarantee T-Shirt availability, and will be cash or check only.</p>
+		    <div style="width:100%; text-align:center;">
+			<iframe src="http://www.eventbrite.com/tickets-external?eid=1147561387&ref=etckt" scrolling="no" frameborder="0" height="224" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+		    </div>
 		</div>
+	    </div>
         </div>
     </div>
     <?php include('footer.php') ?>
