de9b99c5e0ad0be7a45fa756b759be38bc15748e
Bhargav Nookala <bnooka2@yt.acm.uiuc.edu>
2011-08-31 20:28:42 -0500

    fixed registration page maybe?

--- register.php
+++ register.php
@@ -16,8 +16,6 @@ include('header.php');
 	<div id="wrapper">
 		<?php getHeader(); ?>
         <div id="content">
-		<div style="width:100%; text-align:center;" ><iframe  src="http://www.eventbrite.com/tickets-external?eid=1147561387&ref=etckt" frameborder="0" height="224" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe><div style="font-family:Helvetica, Arial; font-size:10px; padding:5px 0 5px; margin:2px; width:100%; text-align:left;" ><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.com/features?ref=etckt" >Online event registration</a><span style="color:#ddd;" > for </span><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://rp.eventbrite.com?ref=etckt" >Reflections | Projections 2011</a><span style="color:#ddd;" > powered by </span><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.com?ref=etckt" >Eventbrite</a></div></div>
-	    <?php getHeader(); ?>
 	    <div id="content">
 		<div style="width:100%; text-align:left;" >
 		    <iframe src="http://www.eventbrite.com/tickets-external?eid=1147561387&ref=etckt" scrolling="no" frameborder="0" height="224" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
