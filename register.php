eff5fe641d5e877c51deaf1953694ec74d9bb12c
Bhargav Nookala <nooknb@gmail.com>
2011-08-31 23:25:59 -0500

    refactored registration page

--- register.php
+++ register.php
@@ -1,31 +1,17 @@
 <?php
 include('header.php');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>About</title>
-    
-    <link href="css/styles.css" rel="stylesheet" type="text/css" />
-    <link href="css/menu.css" rel="stylesheet" type="text/css" />
-    <link href="css/footer.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
-	<div id="wrapper">
-	    <?php getHeader(); ?>
-	    <div id="content-wrapper">
-		<h1 class="registration-heading vertical-text">registration</h1>
-		<div id="content">
-		    <p>Walk-up registration is available, but does not guarantee T-Shirt availability, and will be cash or check only.</p>
-		    <div style="width:100%; text-align:center;">
-			<iframe src="http://www.eventbrite.com/tickets-external?eid=1147561387&ref=etckt" scrolling="no" frameborder="0" height="224" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
-		    </div>
+    <div id="wrapper">
+	<?php getHeader(); ?>
+	<div id="content-wrapper">
+	    <h1 class="registration-heading vertical-text">registration</h1>
+	    <div id="content">
+		<p>Walk-up registration is available, but does not guarantee T-Shirt availability, and will be cash or check only.</p>
+		<div style="width:100%; text-align:center;">
+		    <iframe src="http://www.eventbrite.com/tickets-external?eid=1147561387&ref=etckt" scrolling="no" frameborder="0" height="300px" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
 		</div>
 	    </div>
-        </div>
+	</div>
     </div>
-    <?php include('footer.php') ?>
-</body>
-</html>
+<?php include('footer.php') ?>
+<?php include('close-page.php') ?>
