61b37e732002705ce3e5b42231eb0aca52651699
Mo Kudeki <melisa@mobile-103-187.near.uiuc.edu>
2011-09-03 21:09:01 -0500

    basic schedule content

--- schedule.php
+++ schedule.php
@@ -3,16 +3,80 @@
 	include('header.php');
 ?>
 <div id="wrapper">
-	<?php getHeader(); ?>
-	<div id="content-wrapper">
-		<h1 class="schedule-heading vertical-text">schedule</h1>
-		<div id="content">
-			<h2>Friday</h2>
-			<p class="tab">The schedule for this year is not yet available. Please check back again later.</p>
-			<h2>Saturday</h2>
-			<h2>Sunday</h2>
-		</div><!-- #content end -->
+   <?php getHeader(); ?>
+      <div id="content-wrapper">
+         <h1 class="schedule-heading vertical-text">schedule</h1>
+            <div id="content">
+               <h2>Friday</h2>
+       
+                  <table style="width: 100%; text-align:center" border="1px solid">
+ 
+                     <tr>
+                        <td width="90px">10:00 AM</td>
+                        <td width="400px">
+                          <a href="/Conferenceware/Schedule/Details/19">Reflections | Projections Job Fair</a><br />
+                          Siebel Center Atrium
+                        </td>
+                     </tr>
+    
+    
+                     <tr>
+                        <td>4:00 PM</td>
+                        <td>
+                           <a href="/Conferenceware/Schedule/Details/26">The Future of Programming</a><br />
+                           <a href="/Conferenceware/SpeakerInfo/Display/482">Stephen Wolfram</a><br />
+                           2405 Siebel Center
+                        </td>
+                     </tr>
+    
+    
+                     <tr>
+                        <td>5:00 PM</td>
+                        <td>
+                           <a href="/Conferenceware/Schedule/Details/1">Why Paris Hilton is Famous and Barack Obama is President</a><br />
+                           <a href="/Conferenceware/SpeakerInfo/Display/435">Matt Cheney</a> & 
+                           <a href="/Conferenceware/SpeakerInfo/Display/436">Vilas Dhar</a><br /> 
+                           1404 Siebel Center
+                        </td>
+                        <td>
+                           <a href="/Conferenceware/Schedule/Details/2">Academic Stimulus Package</a><br />
+                           <a href="/Conferenceware/SpeakerInfo/Display/42">Jorge Cham</a><br /> 
+                           1404 Siebel Center
+                        </td>
+                     </tr>
+    
+    
+                     <tr>
+                        <td>6:15 PM</td>
+                        <td>
+                           <a href="/Conferenceware/Schedule/Details/20">Dinner from Noodles &amp; Co. sponsored by Palantir</a><br />
+                           Siebel Center Atrium
+                        </td>
+                     </tr>
+    
+    
+                     <tr>
+                        <td>7:45 PM</td>
+                        <td>
+                           <a href="/Conferenceware/Schedule/Details/2">Academic Stimulus Package</a><br />
+                           <a href="/Conferenceware/SpeakerInfo/Display/42">Jorge Cham</a><br /> 
+                           1404 Siebel Center
+                        </td>
+                     </tr>
+    
+    
+                     <tr>
+                        <td>9:00 PM</td>
+                        <td>
+                           <a href="/Conferenceware/Schedule/Details/13">MechMania Opening Ceremony</a><br />
+                           1404 Siebel Center
+                        </td>
+                     </tr>
+    </table>
+
+      </div><!-- #content end -->
 	</div><!-- #content-wrapper end -->
 </div><!-- #wrapper end -->
+
 <?php include('footer.php') ?>
 <?php include('close-page.php') ?>
