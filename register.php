659a1af1b537d1913e8f160390f5d6afad57b7f8
Bhargav Nookala <nooknb@gmail.com>
2011-09-18 22:33:44 -0500

    fixed height issues on registration pages

--- register.php
+++ register.php
@@ -9,7 +9,7 @@
 	    <div id="content">
 		<p>Walk-up registration is available, but does not guarantee T-Shirt availability, and will be cash or check only.</p>
 		<div style="width:100%; text-align:center;">
-		    <iframe src="http://www.eventbrite.com/tickets-external?eid=1147561387&ref=etckt" scrolling="no" frameborder="0" height="300px" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+		    <iframe src="http://www.eventbrite.com/tickets-external?eid=1147561387&ref=etckt" scrolling="no" frameborder="0" height="500px" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
 		</div>
 	    </div>
 	</div>
