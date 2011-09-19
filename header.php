d88b915b57fecb9d90b7e23ff813ee35ef100186
Hristo Oskov <HristoOskov@mobile-107-163.near.uiuc.edu>
2011-09-18 22:21:12 -0500

    header php fix

--- header.php
+++ header.php
@@ -61,6 +61,7 @@ function getTopInfo($title = null) {
 	
 		<!-- Styles -->
 		<link href="css/styles.css" rel="stylesheet" type="text/css" />
+		<link href="css/schedule.css" rel="stylesheet" type="text/css" />
 		<link href="css/footer.css" rel="stylesheet" type="text/css" />
 		<link href="css/menu.css" rel="stylesheet" type="text/css" />
 		<link href="css/orbit-1.2.3.css" rel="stylesheet" type="text/css" />
@@ -70,19 +71,3 @@ function getTopInfo($title = null) {
 EOT;
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php if (isset($title)) { echo $title . " &laquo; "; } ?>Reflections | Projections 2011</title>
-
-	<!-- Styles -->
-	<link href="css/styles.css" rel="stylesheet" type="text/css" />
-	<link href="css/footer.css" rel="stylesheet" type="text/css" />
-	<link href="css/menu.css" rel="stylesheet" type="text/css" />
-	<link href="css/schedule.css" rel="stylesheet" type="text/css" />
-	<link href="css/orbit-1.2.3.css" rel="stylesheet" type="text/css" />
-	<link href="js/countdown/jquery.countdown.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
