fc8c24e7f7602b1b3ffce1904a98025d9586bb42
Hristo Oskov <HristoOskov@mobile-96-110.near.uiuc.edu>
2011-09-21 20:47:04 -0500

    speaker bios ready

--- speaker_full_page.php
+++ speaker_full_page.php
@@ -15,11 +15,11 @@
 				<div id="i-speaker-pic">
 					<img src="images/speakers/original/<?php echo $speaker->getFullPhoto(); ?>" alt="<?php echo $speaker->getName(); ?>" width="250" />
 				</div>
-				<!-- 
 				<div id="i-speaker-bio">
 					<h2>Bio</h2>
-					<p><?php echo $speaker->getBio(); ?></p>
+					<p><?php echo nl2br($speaker->getBio()); ?></p>
 				</div>
+				<!-- 
 				<div id="i-speaker-talks">
 					<h2>Talk Abstract</h2>
 					<p><?php echo $speaker->getBio(); ?></p>
@@ -30,16 +30,4 @@
 	</div><!-- #content-wrapper end -->
 </div><!-- #wrapper end -->
 <?php include('footer.php') ?>
-
-<script type="text/javascript">
-	$(document).ready(function() {
-		$('.speaker').hover(function() {
-			$(this).find('a:last').trigger('hover');
-			$(this).css('background-color', '#ff0000');
-		}, function() {
-			$(this).css('background-color', '#ffffff');
-		});
-	});
-
-</script>
 <?php include('close-page.php') ?>
