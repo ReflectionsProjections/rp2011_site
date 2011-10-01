030f0c61956053f080990cecf79335c7121b5831
Kevin Lange <k@dakko.us>
2011-09-30 21:46:18 -0500

    More footnotes

--- mechmania.php
+++ mechmania.php
@@ -16,7 +16,7 @@
 			<h3>Corporate Competition</h3>
 			<p>The corporate competition is open to teams representing companies. This year, we have already received enough of a response to ensure a corporate game.</p>
 			<h2>What you need to know</h2>
-			<p>We expect competitors to be familiar with a Linux command line (you will have an option between <i>bash</i>, <i>zsh</i>, and <i>csh</i>). Our provided workstations will also give you <i>vim</i>, <i>emacs</i>, and various graphical editors such as <i>Eclipse</i> and <i>gedit</i>. We will also provide compilation tools for <b>Java</b>, a complete <b>Python 2.7</b> environment, as well as <b>gcc for C, C++</b> and all the other languages it compiles. Sample game clients will be provided in <b>Python and Java</b>, but you are free to use any language capable of communicating with our API. The intermediate data description language will be JSON, so be sure to learn a JSON library for your chosen language.</p>
+			<p>We expect competitors to be familiar with a Linux command line (you will have an option between <i>bash</i>, <i>zsh</i>, and <i>csh</i>). Our provided workstations will also give you <i>vim</i>, <i>emacs</i>, and various graphical editors such as <i>Eclipse</i> and <i>gedit</i>. We will also provide compilation tools for <b>Java</b>, a complete <b>Python 2.7</b> environment, as well as <b>gcc for C, C++</b> and all the other languages it compiles. Sample game clients will be provided in <b>Python and Java</b>, but you are free to use any language capable of communicating with our API<sup>1</sup>. The intermediate data description language will be JSON, so be sure to learn a JSON library for your chosen language.</p>
 			<h2>What you don't need to know</h2>
 			<p>No knowledge of graphics programming is required. For the languages we provide sample clients for, no network programming experience is required. Additionally, Dropbox is running a tutorial on REST in Python during the opening ceremony. This is a change from previous years and was requested by numerous contest alumni.</p>
 			<h2>What you'll do</h2>
@@ -25,7 +25,7 @@
 			<h2>When it'll happen</h2>
 			<p>The competition starts with an opening ceremony on Friday, October 7th., after which we will give you the night to rest. The labs open and you can start coding on Saturday morning at 9AM. We will stop accepting updates to your submission at 9AM Sunday, for a total of 24 hours of coding time. The final presentation will happen at 1PM on Sunday.</p>
 			<h2>What we'll give you</h2>
-			<p>Teams will be provided with 2 PCs running CentOS 5. You should write platform-independent code if you select a language other than Java or Python to ensure compatibility with the competition environment, which usually runs on a Mac Pro. We will provide a standard Unix environment, as described above. You will have access to the Internet throughout the competition and are free to find reference materials, documentation and third party libraries<sup>1</sup></p>
+			<p>Teams will be provided with 2 PCs running CentOS 5. You should write platform-independent code if you select a language other than Java or Python to ensure compatibility with the competition environment, which usually runs on a Mac Pro. We will provide a standard Unix environment, as described above. You will have access to the Internet throughout the competition and are free to find reference materials, documentation and third party libraries<sup>2</sup>.</p>
 			<h2>What you can bring</h2>
 			<p>You can bring any reference manuals you wish, whether it be the Art of Computer Programming or the Art of War. But <b>absolutely no outside code</b>. You may, however, bring non-code items like rc files, build scripts, notes, etc.; we still have the large, fuzzy bat from last year and we will make contact with your skull if necessary.</p>
 			<!--
@@ -43,7 +43,8 @@
 				<iframe  src="http://www.eventbrite.com/tickets-external?eid=1520737567&ref=etckt" frameborder="0" height="500px" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" allowtransparency="true" scrolling="no"></iframe>
 			</div>
 			<h2>Footnotes</h2>
-			<p><sup>1</sup> Please note that when we say "third-party libraries" we mean third-party: not ones written by you, your team, or your friends. We will try to make sure that PIP is set up for Python users.</p>
+			<p><sup>1</sup> ... that compiles and runs on our test machine. Note that this machines you should avoid Linux-specific C/C++. Pure Unix-targeted code should suffice.</p>
+			<p><sup>2</sup> Please note that when we say "third-party libraries" we mean third-party: not ones written by you, your team, or your friends. We will try to make sure that PIP is set up for Python users.</p>
 		</div><!-- #content end -->
 	</div><!-- #content-wrapper end -->
 </div><!-- #wrapper end -->
